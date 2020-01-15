790fc62ba1464bf8c78ccce974fccd0d5bf18081
Philip Whisenhunt <philip.whisenhunt@gmail.com>
2020-01-14 19:39:58 -0500

    Now creating objects from the class I made

--- MyClass.java
+++ MyClass.java
@@ -1,11 +1,11 @@
 public class MyClass{
+    int x =5;
     static void myMethod() {
         System.out.println("I just got executed!");
     }
 
     public static void main(String[] args) {
-        myMethod();
-        myMethod();
-        myMethod();
+        MyClass myObj = new MyClass();
+        System.out.println(myObj.x);
     }
 }
